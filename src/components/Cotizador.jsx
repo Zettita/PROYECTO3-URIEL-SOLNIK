87e938b1a855089f8de3ec078f9a74149a13c6c5
Uriel <uriel.solnik@gmail.com>
2023-11-08 03:14:10 -0300

    otra ruta mas :c

--- src/components/Cotizador.jsx
+++ src/components/Cotizador.jsx
@@ -84,7 +84,7 @@ export default function Cotizador() {
           <button onClick={() => handleSubmit} disabled={loading}>
             {loading ? (
               <img
-                src="./data/Ellipsis-1.1s-44px.gif"
+                src="/src/images/Ellipsis-1.1s-44px.gif"
                 width="80%"
                 alt="Cargando..."
               />
